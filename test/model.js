612458506146779ff8268220c2cdbd51c6a14c42
Jake Harding <jharding@twitter.com>
2012-10-17 22:40:43 -0700

    Revert "Remove mocha test."
    This reverts commit f0a8fff22e3ec6ddea2c079bc8ad7e63b67a27ab.

---
+++ test/model.js
@@ -0,0 +1,104 @@
+var _ = require('underscore');
+var assert = require('assert');
+var sinon = require('sinon');
+var fabio = require('../lib/fabio');
+
+describe('Model', function() {
+  describe('#constructor', function() {
+    it('should call initalize method', function() {
+      var spy = sinon.spy();
+      var Model = fabio.Model.extend({ initialize: spy });
+      var model = new Model();
+
+      assert(spy.called);
+    });
+
+    it('should use defaults', function() {
+      var schema = {
+        prop1: { default: 1 },
+        prop2: { default: 'hi there.' }
+      };
+      var Model = fabio.Model.extend({ schema: schema });
+      var model = new Model();
+
+      _(schema).forEach(function(value, key) {
+        assert.strictEqual(model._attributes[key], value.default);
+      });
+    });
+  });
+
+  describe('#set', function() {
+    var Model, model;
+
+    beforeEach(function() {
+      Model = fabio.Model.extend({
+        schema: {
+          prop1: {},
+          prop2: {},
+          validatedProp: {
+            validator: function(value) { return value > 0; }
+          }
+        }
+      });
+      model = new Model();
+    });
+
+    it('should accept (key, value) signature', function() {
+      model.set('prop1', 1);
+      assert.strictEqual(model._attributes.prop1, 1);
+    });
+
+    it('should accept (object) signature', function() {
+      var attributes = { prop1: 1, prop2: 2 };
+      model.set(attributes);
+
+      _(attributes).forEach(function(value, key) {
+        assert.strictEqual(model._attributes[key], attributes[key]);
+      });
+    });
+
+    it('should throw an error if an attribute fails its validator', function() {
+      assert.throws(function() {
+        model.set('validatedProp', -1);
+      });
+    });
+
+    it('should abort if an attribute fails its validator', function() {
+      var _attributes = _.clone(model._attributes);
+      assert.throws(function() {
+        model.set({ prop1: 1, prop2: 2, validatedProp: -1 });
+      });
+
+      assert.deepEqual(model._attributes, _attributes);
+    });
+
+    it('should act normal if an attribute passes its validator', function() {
+      model.set('validatedProp', 1);
+      assert.strictEqual(model._attributes.validatedProp, 1);
+    });
+
+    it('should be chainable', function() {
+      assert.deepEqual(model, model.set({}));
+    });
+  });
+
+  describe('#get', function() {
+    var Model, model;
+
+    beforeEach(function() {
+      Model = fabio.Model.extend({
+        schema: { prop: {} }
+      });
+      model = new Model();
+    });
+
+    it ('should return the correct value', function() {
+      model.set('prop', 1);
+      assert.strictEqual(model.get('prop'), 1);
+    });
+
+    it('should return undefined if attribute has not been set', function() {
+      assert.strictEqual(model.get('undefined'), undefined);
+    });
+  });
+});
